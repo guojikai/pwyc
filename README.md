8100ec5b08f3f52efba5736de3e07efd9e25ec45
guojikai <guojikai@gmail.com>
2016-02-27 13:09:19 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,34 +1,4 @@
 Pwyc
 ======
-以前流行的最简单的PHP伪原创类，基于词典的同义词替换，现在用不到了。
-
-安装
-----
-使用 Composer 安装：
-
-```
-composer require guojikai/pwyc
-```
-在入口文件引入 Composer 启动脚本： (eg. index.php)
-
-```php
-require 'vendor/autoload.php';
-```
-
-使用
-----
-```php
-<?php
-
-use Pwyc\Pwyc;
-
-$pwyc = new Pwyc;
-$new_text = $pwyc->replace('需要被替换的原文');
-
-var_dump($new_text);
-
-//string(24) "须要被调换的原文"
-
-?>
-```
+以前流行的最简单的PHP伪原创类，基于词典的同义词替换，现在用不到了。放这做个纪念。
 
--- src/Pwyc.php
+++ src/Pwyc.php
@@ -1,37 +1,34 @@
 <?php
 
-namespace Pwyc;
-
-use Exception;
-
 /**
  * Pwyc(PHP伪原创)
  * Author: guojikai
  * Last Modified: 2010-11-28 17:14
  * version 1.0
  */
-class Pwyc {
+class Pwyc
+{
 
-	private $replaced = [];
-  private $synonyms = [];
+    private $replaced = array();
+    private $synonyms = array();
 
-	//construct
-	function __construct()
-	{
-		$this->synonyms = require(dirname(__FILE__).'/dict.php');
-	}
+    //construct
+    function __construct()
+    {
+        $this->synonyms = require(dirname(__FILE__).'/dict.php');
+    }
 
-  //replace
-	function replace($text)
-	{
-		foreach($this->synonyms as $key => $val) {
-			if(preg_match("/".$key."/", $text) && !in_array($key, $this->replaced)) {
-				$text = str_replace($key, $val, $text);
-				array_push($this->replaced, $val);
-			}
-		}
-		return $text;
-	}
+    //replace
+    function replace($text)
+    {
+        foreach($this->synonyms as $key => $val) {
+            if(preg_match("/".$key."/", $text) && !in_array($key, $this->replaced)) {
+                $text = str_replace($key, $val, $text);
+                array_push($this->replaced, $val);
+            }
+        }
+        return $text;
+    }
 
 }
 
